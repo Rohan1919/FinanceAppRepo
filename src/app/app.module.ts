77bbdb6eb24d7d8ffa1847b64416b39d3a625db6
lntinfotech <Varun.Tk.com>
2020-02-19 02:19:51 +0530

    home page work and routing to regster page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
